6a536c72084e9f046593de2d938f11348917c2ad
Aleksa <aleksa.belic@gmail.com>
2021-09-15 13:15:54 -0700

    intl commit

---
+++ src/BigNum/BigNum.java
@@ -0,0 +1,16 @@
+package BigNum;
+
+public class BigNum {
+    public static void BigNum(String arg){
+
+    }
+    public static String toString(double num){
+
+    }
+    public static double plus(double numOne, double numTwo){
+
+    }
+    public static double times(double numOne, double numTwo){
+
+    }
+}
