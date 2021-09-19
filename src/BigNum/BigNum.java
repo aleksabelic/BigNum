1774486b6a33316554a369169f7308152d25cd60
Haley <57505566+HaleyRovner@users.noreply.github.com>
2021-09-19 15:30:34 -0700

    all done!

--- src/BigNum/BigNum.java
+++ src/BigNum/BigNum.java
@@ -1,30 +1,50 @@
+//Haley Rovner, Aleksa Belic, Kendall Malone
 package BigNum;
 
 public class BigNum {
 
-    String value;
+    int value[];
+    int length;
+    long number;
 
     public BigNum(String num){
-        value = num;
+        if(num.equals("")){
+            num="0";
+        }
+        length = num.length();
+        value = new int[length];
+        for(int i = 0; i < length; i++) {
+            value[i] = (num.charAt(i)) - 48;
+            number *= 10;
+            number += value[i];
+        }
     }
-
     public String toString(){
         //method that returns a String representing the number
-        return this.value;
+        String output = "";
+        for(int i = 0; i < this.length; i++){
+            output += (char)(value[i]+48);
+        }
+        return output;
     }
-    public static BigNum plus(BigNum numTwo){
+
+    public BigNum plus(BigNum numTwo){
         //method that takes a second BigNum and returns the sum of the two numbers
         //value of a is this.value
         //value of b is numTwo.value
-        BigNum output = new BigNum("0");
+
+        long result = this.number + numTwo.number;
+        String out = String.format("%d", result);
+        BigNum output = new BigNum(out);
         return output;
 
     }
-    public static BigNum times(BigNum numTwo){
+    public BigNum times(BigNum numTwo){
         //method that takes a second BigNum and returns the product of the two numbers.
-
-        BigNum output = new BigNum("0");
+        long result = this.number * numTwo.number;
+        String out = String.format("%d", result);
+        BigNum output = new BigNum(out);
         return output;
-
     }
+
 }
